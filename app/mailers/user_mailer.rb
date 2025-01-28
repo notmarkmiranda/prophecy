8469c8ed81e781a0478334e0d288ed47f20c8885
Mark Miranda <notmarkmiranda@gmail.com>
2025-01-28 09:39:05 -0700

    chore: update from email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,10 @@
 class UserMailer < ApplicationMailer
   def login_code_email(user)
     @user = user
-    mail(to: @user.email, subject: "Your Login Code")
+    mail(
+      to: @user.email,
+      subject: "Your Login Code",
+      from: "login-code@superduper.bet"
+    )
   end
 end
